03fec385a7f1baedcaddbe91b14a40f2b791d3f6
Ramiro Grisales noguera <ramirogrisales@gmail.com>
2022-08-27 13:29:14 -0500

    test

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const cors = require('cors');
+
 
 app.get('/', (req, res) => res.send('Hello World!, and this is test, prueba pipeline!'));
 
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+app.listen(port, ()=>{
+    console.log(`App running on http://localhost:${port}`);
+});
